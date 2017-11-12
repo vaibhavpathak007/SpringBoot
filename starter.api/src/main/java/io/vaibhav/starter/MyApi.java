44fcd85bcd1c182f3f58a90fddfcfa3d77eeb08c
admin <admin@Toshiba-C55>
2017-11-12 10:34:43 +0530

    First commit with hello world.

---
+++ starter.api/src/main/java/io/vaibhav/starter/MyApi.java
@@ -0,0 +1,15 @@
+package io.vaibhav.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApi {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MyApi.class, args);
+		
+	}
+
+}
---
+++ starter.api/src/main/java/io/vaibhav/starter/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package io.vaibhav.starter.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHello()
+	{
+		return "Hi";
+	}
+	
+}
