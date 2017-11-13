7fd8325533a1561fd18d4097a3c1befe5f7f7cd9
admin <admin@192.168.0.7>
2017-11-13 12:46:54 +0530

    added methods for operations like get all, delete and update topic with methodtype GET, POST, PUT, DELETE.

--- starter.api/src/main/java/io/vaibhav/starter/topic/TopicController.java
+++ starter.api/src/main/java/io/vaibhav/starter/topic/TopicController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,7 +15,7 @@ public class TopicController {
 	@Autowired
 	private TopicService topicservice;
 
-	@RequestMapping("/topics")
+	@RequestMapping(value="/topics",method=RequestMethod.GET)
 	public List<TopicTO> getAllTopics(){
 	
 		return topicservice.getAllTopics();
@@ -22,9 +24,24 @@ public class TopicController {
 	
 	@RequestMapping("/topics/{id}")
 	public TopicTO getTopic(@PathVariable String id){
-		
 		return topicservice.getTopic(id);
-		
 	}
+	
+	@RequestMapping(value="/topics", method=RequestMethod.POST)
+	public void addTopic(@RequestBody TopicTO topic){
+		topicservice.addTopic(topic);	
+	}
+	
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.PUT)
+	public void updateTopic(@RequestBody TopicTO topic, @PathVariable String id){
+		topicservice.updateTopic(topic,id);	
+	}
+	
+	@RequestMapping(value="/topics/{id}", method=RequestMethod.DELETE)
+	public void deleteTopic(@PathVariable String id){
+		topicservice.deleteTopic(id);	
+	}
+	
+	
 	
 }
--- starter.api/src/main/java/io/vaibhav/starter/topic/TopicService.java
+++ starter.api/src/main/java/io/vaibhav/starter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.vaibhav.starter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -7,10 +8,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<TopicTO> topics = Arrays.asList( new TopicTO("spring", "spring framework", "spring framework description"),
+	private List<TopicTO> topics = new ArrayList<TopicTO>(Arrays.asList( new TopicTO("spring", "spring framework", "spring framework description"),
 			 new TopicTO("java", "core java", "core java description"),
 			 new TopicTO("javascript", "javascript", "javascript descrition")
-			 );
+			 ));
 	
 	public List<TopicTO> getAllTopics(){
 		
@@ -25,6 +26,36 @@ public class TopicService {
 			}
 		}
 		return null;
+	}
+
+	public void addTopic(TopicTO topic) {
+		
+		topics.add(topic);
+		
+	}
+
+	public void updateTopic(TopicTO topic, String id) {
+	 for(TopicTO mytopic: topics){
+		 if(mytopic.getId().equals(id)){
+			 mytopic.setId(topic.getId());
+			 mytopic.setName(topic.getName());
+			 mytopic.setDescription(topic.getDescription());
+			 return;
+		 }
+	 }
+		
+	}
+
+	public void deleteTopic(String id) {
+		
+		for(int i=0; i<topics.size(); i++){
+			TopicTO tt = topics.get(i);
+			if(tt.getId().equals(id)){
+				topics.remove(i);
+				return;
+			}
+		}
+		
 	}
 	
 
